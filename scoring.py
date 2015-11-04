d1195eebfad8f1f2a33bf4f81b62ed4ac4ba373e
Joyce Xu <joycexu128@gmail.com>
2015-11-03 23:47:30 -0700

    Update clustering

--- scoring.py
+++ scoring.py
@@ -1,3 +1,21 @@
+import math
+import numpy as np
+
+# Mean squared error
 def mse(classifier, X_train, X_test, y_train, y_test):
     pred = classifier.fit(X_train, y_train).predict(X_test)
     return ((pred - y_test) ** 2).mean()
+
+# Root mean squared error
+def rmse(y_pred, y_true):
+    return math.sqrt(((y_true-y_pred)**2).mean())
+
+# Mean absolute error
+def mape(y_pred, y_true):
+    return np.mean(np.abs((y_true - y_pred) / y_true))
+
+# Percent of predictions within a certain value to true value
+def error_within(epsilon, y_pred, y_true):
+    within = np.absolute(y_true-y_pred) < epsilon
+    return np.sum(within)/within.size
+    
